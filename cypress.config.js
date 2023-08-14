07d9f3298d9cfe0585b77aae761186f6d3d7b7a5
elianmanubens12 <elianmanubens10@gmail.com>
2023-08-14 01:15:44 -0300

    mi primer test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    env: { grepFilterSpecs: true, grepOmitFiltered: true },
+    viewportmobile: {
+      device: "iphone-xr",
+    },
+    viewportdesktop: {
+      device: "macbook-16",
+    },
+  },
   e2e: {
+    baseUrl: "https://www.edenentradas.com.ar/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("@bahmutov/cy-grep/src/plugin")(config);
+      // IMPORTANT: return the config object
+      return config;
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,47 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+Cypress.Commands.add("openWeb", () => {
+  let viewDevice;
+  if (Cypress.env("type") != "mobile") {
+    viewDevice = Cypress.env("viewportdesktop").device;
+  } else {
+    viewDevice = Cypress.env("viewportmobile").device;
+  }
+  cy.log(`**${JSON.stringify(viewDevice)}**`);
+  cy.viewport(viewDevice);
+  cy.visit("/");
+});
+
+Cypress.Commands.add("validarScheme", (schemaJson, servicioJson) => {
+  cy.fixture(`schemas/${schemaJson}.json`).then((schema) => {
+    //cy.log(JSON.stringify(schema));
+    const validate = ajv.compile(schema);
+
+    cy.fixture(`${servicioJson}.json`).then((servJson) => {
+      //cy.log(JSON.stringify(servJson));
+      const data = servJson;
+
+      const valid = validate(data);
+      if (!valid) {
+        cy.log(validate.errors);
+      } else {
+        cy.log(`La respuesta del servicio posee los tipos de datos correctos`);
+      }
+    });
+  });
+});
+
+Cypress.Commands.add("serviceCall", (metodo, urly, fileName, schemaName) => {
+  cy.request({
+    method: metodo,
+    url: urly,
+  }).then((response) => {
+    cy.writeFile(`cypress/fixtures/${fileName}.json`, response.body);
+    expect(response.status).to.eq(200);
+    cy.validarScheme(schemaName, fileName);
+  });
+});
