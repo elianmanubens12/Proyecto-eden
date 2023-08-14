35cfea7978d4af4061b67493186d8f117d6855d9
elianmanubens12 <elianmanubens10@gmail.com>
2023-08-14 01:24:28 -0300

    mi primer test

--- cypress.config.js
+++ cypress.config.js
@@ -10,13 +10,5 @@ module.exports = defineConfig({
       device: "macbook-16",
     },
   },
-  e2e: {
-    baseUrl: "https://www.edenentradas.com.ar/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require("@bahmutov/cy-grep/src/plugin")(config);
-      // IMPORTANT: return the config object
-      return config;
-    },
-  },
+  
 });
